c4f61f1172b64e79238bdd2a72b0029b6da9b20d
Chang Liu <chl282@pitt.edu>
2019-11-07 15:04:47 -0500

    split function

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -5,6 +5,11 @@ a python package for the workshop. molecules analyzing and visulizing
 
 # Add imports here
 from .functions import *
+from .measure import calculate_distance
+from .molecule import build_bond_list
+from .visualize import draw_molecule, bond_histogram
+
+
 
 # Handle versioneer
 from ._version import get_versions
